896154270ab8263a4fbfbf42d6d2da0e74042dc4
ommirandap <ommirandap@gmail.com>
2014-05-23 03:06:40 -0400

    coding

---
+++ LowDiscrepancyPointGenerator.java
@@ -0,0 +1,18 @@
+
+public class LowDiscrepancyPointGenerator extends PointGenetaror {
+
+	Point []result;
+
+	public LowDiscrepancyPointGenerator(int i){
+		super(i);
+	}
+
+	@Override
+	public Point[] generateInstance(){
+
+		result = new Point[size];
+
+		
+	}
+
+}
