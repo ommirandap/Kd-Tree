c749fc99d6d6b761dfa8fcb5fe2fa19659a80a74
ommirandap <ommirandap@gmail.com>
2014-06-07 00:27:23 -0400

    low discrepancy

--- LowDiscrepancyPointGenerator.java
+++ LowDiscrepancyPointGenerator.java
@@ -1,5 +1,5 @@
 
-public class LowDiscrepancyPointGenerator extends PointGenetaror {
+public class LowDiscrepancyPointGenerator extends PointGenerator {
 
 	Point []result;
 
@@ -12,7 +12,34 @@ public class LowDiscrepancyPointGenerator extends PointGenetaror {
 
 		result = new Point[size];
 
-		
+		for(int index = 0; index < size; index++){
+			double x = getHaltonNumber(index, 2)*(max_range);
+			double y = getHaltonNumber(index, 3)*(max_range);
+			result[index] = new Point(x, y, index);
+		}
+
+		return result;	
 	}
 
-}
+    public double getHaltonNumber(int index, int primeBase){
+
+    	if(index < 0)
+    		index = 0;
+
+    	index++;
+
+    	double res = 0;
+    	double factor = 1.0/primeBase;
+       	
+       	int i = index;
+       	
+       	while(i > 0)
+       	{
+       		res = res + factor * (i % primeBase);
+       		i = i / primeBase;
+       		factor = factor / primeBase;
+       	}
+       	return res;
+    }
+
+   }
--- Testest.java
+++ Testest.java
@@ -4,16 +4,17 @@ public class Testest {
     static public void main(String []args){
 
         PointGenerator GI;
-        GI = new RandomPointGenerator(20);
+        //GI = new RandomPointGenerator(5);
+        GI = new LowDiscrepancyPointGenerator(7);
 
 		Point DATA[];
+
         DATA = GI.generateInstance();
         System.out.println(GI.toString());
 
 		for(int i = 0; i < DATA.length; i++){
             System.out.print("Point N°: "+i+" -> ");
             System.out.println(DATA[i].toString());
-            System.out.println(DATA[i].distanceTo(new Point(0,0)));
 		}
     }
 }
